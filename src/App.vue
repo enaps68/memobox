2086103020073708c30ae760d6edb167fa528524
enaps68 <114097252+enaps68@users.noreply.github.com>
2025-01-19 16:11:50 +0100

    Inicijalizacija projekta

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/login">Prijava</router-link> |
+      <router-link to="/signup">Registracija</router-link>
     </nav>
     <router-view/>
   </div>
