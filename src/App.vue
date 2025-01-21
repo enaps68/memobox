70c3c87fff2ee52dba9423a0e7666775bc23e8bb
enaps68 <114097252+enaps68@users.noreply.github.com>
2025-01-21 18:10:00 +0100

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">Prijava</router-link> |
-      <router-link to="/signup">Registracija</router-link>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand">
+          <img
+            src="@/assets/myicon.png"
+            alt="Logo"
+            width="30"
+            class="d-inline-block align-text-top"
+          />
+          Memobox
+        </a>
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/login">Prijava</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/signup"
+                >Registracija</router-link
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,26 +1,37 @@
 <template>
   <div class="about">
-  <h1>Prijavi se</h1>
-  <div class="container">
-  <div class="row">
-  <div class="col-sm"></div>
-  <div class="col-sm">
-  <form>
-  <div class="form-group">
-  <label for="exampleInputEmail1">Email adresa</label>
-  <input type="email" class="form-control"
-  id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Upiši email" />
+    <br />
+    <h1>Prijavi se</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email adresa</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Upiši email"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Lozinka</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Upiši lozinku"
+              />
+            </div>
+            <br />
+            <button type="submit" class="btn btn-primary">Nastavi</button>
+          </form>
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
   </div>
-  <div class="form-group">
-  <label for="exampleInputPassword1">Lozinka</label>
-  <input type="password" class="form-control"
-  id="exampleInputPassword1" placeholder="Upiši lozinku" />
-  </div>
-  <button type="submit" class="btn btn-primary">Nastavi</button>
-  </form>
-  </div>
-  <div class="col-sm"></div>
-  </div>
-  </div>
-  </div>
-  </template>
+</template>
